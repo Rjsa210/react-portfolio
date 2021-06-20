26aab9320906a3a2da656a4f781bce4d5ae661c0
Reid Johnson <rjsa210@gmail.com>
2021-06-19 21:25:10 -0700

    submitting with errors in portfolio -RJ

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Unit 20 REACT Homework: React Portfolio
+## Description
+This is a web application built using React.
+
+## Use
+This is a website generated with the REACT framework
+
+## Deployment
+
+### [Github Repository](https://github.com/Rjsa210/react-portfolio)
+### [Github Pages](https://rjsa210.github.io/react-portfolio/)
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -6,7 +6,7 @@ function ProjectCard(props) {
 
     <div className="col-sm-6 col-md-6 col-lg-6">
       <figure className="figure project-image" >
-        <a href={props.url}><img src={'./assets' + props.image}
+        <a href={props.url}><img src={'./src/assets' + props.image}
           className="figure-img img-fluid rounded portfolio-img" alt={props.title}/></a>
         <figcaption className="figure-caption">{props.title}: {props.description}</figcaption>
       </figure>
